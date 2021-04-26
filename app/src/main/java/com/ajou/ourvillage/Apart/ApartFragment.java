3432a2acfd4981cb5f6ac8c3d0608dc7bf393beb
SH0610 <sanghee0610@ajou.ac.kr>
2021-04-26 21:30:20 +0900

    아파트 탭에서 각각의 3가지 메뉴(공지사항 액티비티, 건의사항 액티비티, 커뮤니티 액티비티)로 이동하도록 구현

--- app/src/main/java/com/ajou/ourvillage/Apart/ApartFragment.java
+++ app/src/main/java/com/ajou/ourvillage/Apart/ApartFragment.java
@@ -1,5 +1,6 @@
 package com.ajou.ourvillage.Apart;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,56 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.ajou.ourvillage.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ApartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ApartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btn_notice, btn_help, btn_community;
 
     public ApartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ApartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ApartFragment newInstance(String param1, String param2) {
-        ApartFragment fragment = new ApartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apart, container, false);
+        View view = inflater.inflate(R.layout.fragment_apart, container, false);
+
+        btn_notice = view.findViewById(R.id.apart_btn_more_notice);
+        btn_notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ApartNoticeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_help = view.findViewById(R.id.apart_btn_more_help);
+        btn_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ApartHelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_community = view.findViewById(R.id.apart_btn_more_community);
+        btn_community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ApartCommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
