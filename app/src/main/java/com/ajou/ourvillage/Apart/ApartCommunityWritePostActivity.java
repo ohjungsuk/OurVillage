289709442001e560122f602e34b21eebf43fb3bc
SH0610 <sanghee0610@ajou.ac.kr>
2021-05-01 22:36:00 +0900

    아파트 커뮤니티 탭의 공지사항, 커뮤니티 탭에서 글쓰기 UI 구현

---
+++ app/src/main/java/com/ajou/ourvillage/Apart/ApartCommunityWritePostActivity.java
@@ -0,0 +1,47 @@
+package com.ajou.ourvillage.Apart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ajou.ourvillage.R;
+
+public class ApartCommunityWritePostActivity extends AppCompatActivity {
+
+    private Button btn_close;
+    private TextView btn_write;
+    private EditText et_content;
+    private String content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apart_community_write_post);
+
+        btn_close = findViewById(R.id.apart_community_write_post_btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "글쓰기가 취소되었습니다.", Toast.LENGTH_SHORT);
+                finish();
+            }
+        });
+
+        et_content = findViewById(R.id.apart_community_write_post_tv_content);
+
+        btn_write = findViewById(R.id.apart_community_write_post_btn_write);
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                content = et_content.getText().toString();
+                System.out.println("입력 내용 : " + content);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/ajou/ourvillage/Apart/ApartNoticeActivity.java
+++ app/src/main/java/com/ajou/ourvillage/Apart/ApartNoticeActivity.java
@@ -2,15 +2,21 @@ package com.ajou.ourvillage.Apart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ajou.ourvillage.R;
 
 public class ApartNoticeActivity extends AppCompatActivity {
 
     private Button btn_close;
+    private TextView btn_search, btn_write;
+    private EditText et_search;
+    private String search_keyword = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,23 @@ public class ApartNoticeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn_search = findViewById(R.id.apart_notice_btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search_keyword = et_search.getText().toString();
+            }
+        });
+
+        btn_write = findViewById(R.id.apart_notice_btn_write);
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ApartCommunityWritePostActivity.class);
+                startActivity(intent);
             }
         });
     }
