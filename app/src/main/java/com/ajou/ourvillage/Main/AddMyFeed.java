9e55bafc7874032924eccef7a1e15925de7382fe
ohjungsuk <dtps@ajou.ac.kr>
2021-05-21 21:38:26 +0900

    갤러리 액티비티 구현. 게시글에 갤러리 이미지 올리기 구현중.

--- app/src/main/java/com/ajou/ourvillage/Main/AddMyFeed.java
+++ app/src/main/java/com/ajou/ourvillage/Main/AddMyFeed.java
@@ -3,9 +3,13 @@ package com.ajou.ourvillage.Main;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -31,7 +35,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class AddMyFeed extends AppCompatActivity {
     private static final String TAG = "AddMyFeed";
 
-    private ImageButton btn_backToMain;
+    private ImageButton btn_backToMain,imgbtn_upload;
     private Button btn_feed_write;
     private EditText edt_feed_comment,edt_feed_title;
     private FirebaseFirestore db;
@@ -51,6 +55,7 @@ public class AddMyFeed extends AppCompatActivity {
         edt_feed_title = (EditText)findViewById(R.id.edt_feed_title);
         btn_feed_write = (Button)findViewById(R.id.btn_feed_write);
         btn_backToMain = (ImageButton)findViewById(R.id.btn_backToMain);
+        imgbtn_upload = (ImageButton)findViewById(R.id.imgbtn_upload);
     }
 
     public void btnMover(){
@@ -80,6 +85,42 @@ public class AddMyFeed extends AppCompatActivity {
                 post();
             }
         });
+        imgbtn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //권한이 없을떄
+                if(ContextCompat.checkSelfPermission(AddMyFeed.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(AddMyFeed.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                        ActivityCompat.requestPermissions(AddMyFeed.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    }else{ //권한 다시 물어보는 경우
+                        ActivityCompat.requestPermissions(AddMyFeed.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                        Toast.makeText(getApplicationContext(),
+                                "권한을 허용해주세요", Toast.LENGTH_SHORT).show();
+                    }
+                }else{//권한이 있을때
+                    Intent intent = new Intent(AddMyFeed.this,GalleryAcitivity.class);
+                    startActivity(intent);
+                    //finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(AddMyFeed.this, GalleryAcitivity.class);
+                    startActivity(intent);
+                    //finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "권한을 허용해주세요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
     private void post(){
