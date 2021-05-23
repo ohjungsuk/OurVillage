a0066c513cfc4509e1b0a0ff7222ed77b29a7e36
SH0610 <sanghee0610@ajou.ac.kr>
2021-05-23 14:24:43 +0900

    아파트 탭 수정

--- app/src/main/java/com/ajou/ourvillage/Apart/ApartFragment.java
+++ app/src/main/java/com/ajou/ourvillage/Apart/ApartFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.ajou.ourvillage.R;
+
+import java.util.ArrayList;
 
 public class ApartFragment extends Fragment {
 
@@ -31,34 +35,31 @@ public class ApartFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_apart, container, false);
 
-        btn_notice = view.findViewById(R.id.apart_btn_more_notice);
-        btn_notice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), ApartNoticeActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        btn_help = view.findViewById(R.id.apart_btn_more_help);
-        btn_help.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), ApartHelpActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_community = view.findViewById(R.id.apart_btn_more_community);
-        btn_community.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), ApartCommunityActivity.class);
-                startActivity(intent);
-            }
-        });
 
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        RecyclerView recyclerView = getActivity().findViewById(R.id.apart_recyclerview);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecyclerViewDecoration spaceDecoration = new RecyclerViewDecoration(30);
+        recyclerView.addItemDecoration(spaceDecoration);
+
+        ArrayList<ApartPostItem> dataList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            dataList.add(new ApartPostItem(R.drawable.ic_launcher_background, "이름임", "2021년 5월 3일 월요일", "맛집 찾음", "요 앞에 새로 생긴 곳 마싯더라구영", "5", "3", false));
+        }
+
+        ApartPostAdapter apartPostAdapter = new ApartPostAdapter(dataList);
+        recyclerView.setAdapter(apartPostAdapter);
+        recyclerView.getAdapter().notifyDataSetChanged();
+    }
+
 
 }
